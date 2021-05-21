080b8ab11af87ed7771989bc1923737b8e7fd7da
F_Punk <fabiopungg@gmail.com>
2021-05-21 13:48:09 +0200

    looks ok so far

--- src/lib.rs
+++ src/lib.rs
@@ -2,68 +2,69 @@
 
 #![no_std]
 
-use cortex_m::peripheral::{syst::SystClkSource, DCB, DWT, SYST};
+use hal::timer::Instance;
+use nrf52840_hal as hal;
 use rtic_monotonic::{
     embedded_time::{clock::Error, fraction::Fraction},
     Clock, Instant, Monotonic,
 };
-
 /// DWT and Systick combination implementing `embedded_time::Clock` and `rtic_monotonic::Monotonic`
 ///
 /// The frequency of the DWT and SysTick is encoded using the parameter `FREQ`.
-pub struct DwtSystick<const FREQ: u32> {
-    dwt: DWT,
-    systick: SYST,
+pub struct NrfMonotonic<INSTANCE: Instance> {
+    timer: INSTANCE,
 }
 
-impl<const FREQ: u32> DwtSystick<FREQ> {
+impl<INSTANCE: Instance> NrfMonotonic<INSTANCE> {
     /// Enable the DWT and provide a new `Monotonic` based on DWT and SysTick.
     ///
     /// Note that the `sysclk` parameter should come from e.g. the HAL's clock generation function
     /// so the real speed and the declared speed can be compared.
-    pub fn new(dcb: &mut DCB, dwt: DWT, systick: SYST, sysclk: u32) -> Self {
-        assert!(FREQ == sysclk);
-
-        dcb.enable_trace();
-        DWT::unlock();
-
-        unsafe { dwt.cyccnt.write(0) };
-
+    pub fn new(instance: INSTANCE) -> Self {
+        {
+            // set up the peripheral
+            let t0 = instance.as_timer0();
+            t0.bitmode.write(|w| w.bitmode()._32bit());
+            t0.events_compare[0].reset();
+            t0.mode.write(|w| w.mode().timer());
+            // 1MHz
+            t0.prescaler.write(|w| unsafe { w.prescaler().bits(4) });
+            t0.tasks_clear.write(|w| w.tasks_clear().set_bit());
+        }
         // We do not start the counter here, it is started in `reset`.
 
-        DwtSystick { dwt, systick }
+        NrfMonotonic { timer: instance }
     }
 }
 
-impl<const FREQ: u32> Clock for DwtSystick<FREQ> {
+impl<INSTANCE: Instance> Clock for NrfMonotonic<INSTANCE> {
     type T = u32;
 
-    const SCALING_FACTOR: Fraction = Fraction::new(1, FREQ);
+    const SCALING_FACTOR: Fraction = Fraction::new(1, 1);
 
     fn try_now(&self) -> Result<Instant<Self>, Error> {
-        // The instant is always valid when the DWT is not reset
-        Ok(Instant::new(self.dwt.cyccnt.read()))
+        Ok(Instant::new(self.timer.read_counter()))
     }
 }
 
-impl<const FREQ: u32> Monotonic for DwtSystick<FREQ> {
-    const DISABLE_INTERRUPT_ON_EMPTY_QUEUE: bool = true;
+impl<INSTANCE: Instance> Monotonic for NrfMonotonic<INSTANCE> {
+    const DISABLE_INTERRUPT_ON_EMPTY_QUEUE: bool = false;
 
     unsafe fn reset(&mut self) {
-        self.dwt.enable_cycle_counter();
-
-        self.systick.set_clock_source(SystClkSource::Core);
-        self.systick.enable_counter();
-
-        self.dwt.cyccnt.write(0);
+        {
+            let t0 = self.timer.as_timer0();
+            t0.tasks_stop.write(|w| w.tasks_stop().set_bit());
+            t0.tasks_clear.write(|w| w.tasks_clear().set_bit());
+            t0.tasks_start.write(|w| w.tasks_start().set_bit());
+        }
     }
 
     fn set_compare(&mut self, val: &Instant<Self>) {
         // The input `val` is in the timer, but the SysTick is a down-counter.
         // We need to convert into its domain.
-        let now: Instant<Self> = Instant::new(self.dwt.cyccnt.read());
+        let now: Instant<Self> = Instant::new(self.timer.read_counter());
 
-        let max = 0x00ff_ffff;
+        let max = u32::MAX;
 
         let dur = match val.checked_duration_since(&now) {
             None => 1, // In the past
@@ -75,11 +76,11 @@ impl<const FREQ: u32> Monotonic for DwtSystick<FREQ> {
             Some(x) => max.min(*x.integer()).max(1),
         };
 
-        self.systick.set_reload(dur);
-        self.systick.clear_current();
+        self.timer.as_timer0().cc[0].write(|w| unsafe { w.cc().bits(dur) });
     }
 
     fn clear_compare_flag(&mut self) {
+        self.timer.timer_reset_event();
         // NOOP with SysTick interrupt
     }
 }
