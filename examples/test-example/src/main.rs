064e90ecc2282a275e8a121dced2da99120d55a7
Fabio Pungg <fabio.pungg@medel.com>
2021-05-25 13:44:20 +0200

    finished example, timer works

--- examples/test-example/src/main.rs
+++ examples/test-example/src/main.rs
@@ -29,21 +29,35 @@ defmt::timestamp!("{=usize}", COUNT.fetch_add(1, Ordering::Relaxed));
 
 #[app(device = crate::hal::pac, dispatchers = [PWM0])]
 mod APP {
+    use crate::hal;
+    use hal::gpio::{Level, Output, Pin, PushPull};
+    use hal::prelude::{OutputPin, StatefulOutputPin, ToggleableOutputPin};
     use nrf_monotonic::NrfMonotonic;
-    use rtic::time::duration::Seconds;
+    use rtic::time::duration::{Milliseconds, Seconds};
 
     #[monotonic(binds = TIMER0, default = true)]
     type MyMono = NrfMonotonic<crate::hal::pac::TIMER0>;
+
+    #[resources]
+    struct Resources {
+        #[task_local]
+        led: crate::hal::gpio::Pin<Output<PushPull>>,
+        #[task_local]
+        pin: crate::hal::gpio::Pin<Output<PushPull>>,
+    }
 
     #[init]
     fn init(cx: init::Context) -> (init::LateResources, init::Monotonics) {
         defmt::info!("init");
 
         let mono = NrfMonotonic::new(cx.device.TIMER0);
+        let port0 = hal::gpio::p0::Parts::new(cx.device.P0);
+        let led = port0.p0_13.into_push_pull_output(Level::High).degrade();
+        let pin = port0.p0_31.into_push_pull_output(Level::High).degrade();
 
         tttask::spawn_after(Seconds(1_u32)).unwrap();
-
-        (init::LateResources {}, init::Monotonics(mono))
+        blink_led::spawn_after(Milliseconds(500_u32)).unwrap();
+        (init::LateResources { led, pin }, init::Monotonics(mono))
     }
 
     #[idle]
@@ -55,6 +69,21 @@ mod APP {
         loop {
             cortex_m::asm::wfi();
         }
+    }
+
+    #[task(resources = [led, pin])]
+    fn blink_led(cx: blink_led::Context) {
+        let led = cx.resources.led;
+        let pin = cx.resources.pin;
+
+        if led.is_set_high().unwrap() {
+            led.set_low().unwrap();
+            pin.set_low().unwrap();
+        } else {
+            led.set_high().unwrap();
+            pin.set_high().unwrap();
+        }
+        blink_led::spawn_after(Milliseconds(500_u32)).unwrap();
     }
 
     #[task]
